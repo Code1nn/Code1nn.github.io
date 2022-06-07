c4546ed22fd0c620d4d6233629712adde61d20c5
Code1nn <101986371+Code1nn@users.noreply.github.com>
2022-06-07 22:50:30 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -1,13 +1,7 @@
 'use strict';
-function getDecimal(num) {
-    let a = num.slice(num.indexOf("."));
-    let str = 0 + a;
-    if (Number(num) < 0) {
-        str = 1 - Number(str);
-        str = str.toFixed(a.length - 1);
-    }
-    return str;
-}
+function getDecimal(num){
+                return num - Math.floor(num); //округление вниз 
+            }
 
 function ucFirst(str) {
     return (str[0].toUpperCase() + str.slice(1));
