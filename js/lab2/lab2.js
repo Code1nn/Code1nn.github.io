0fda1dce1e5e39a91fcd6b1a4e11c4e52c53931b
Code1nn <101986371+Code1nn@users.noreply.github.com>
2022-06-08 11:02:41 +0300

    Update lab2.js

--- js/lab2/lab2.js
+++ js/lab2/lab2.js
@@ -54,8 +54,8 @@ function factorial(n){
  * @returns y>x?
  */
 function compare(x){
-  let y=prompt("введите число У");
-  let res=(function(y){
+  let res=(function(){
+    let y=5;
     if (y>x) return true
       else if (y<x) return false
 	else return null;
